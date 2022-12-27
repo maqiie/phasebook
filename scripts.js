acfc325be71bc6d7c43c6a272be6a30c64abe1f9
maqiie <99504010+maqiie@users.noreply.github.com>
2022-12-27 13:20:24 +0300

    styled the cards

---
+++ scripts.js
@@ -0,0 +1,22 @@
+function getData(){
+    fetch("http://localhost:3000/messages")
+    .then(response=> response.json())
+    .then(jsonData=>displayData(jsonData))
+    
+}
+function displayData(data){
+    console.log(getData);
+    for (item of data){
+        const card =document.createElement("div")
+        card.className="card"
+        const cardDetails=`
+        <img src="${item.image}" alt="student image">
+        <p>${item.message}"</p>
+        <h4>${item.name}"</h4>
+        `
+        card.innerHTML=cardDetails
+        const conatiner=document.getElementById("data_container")
+        conatiner.appendChild(card)
+   }
+}
+getData()
